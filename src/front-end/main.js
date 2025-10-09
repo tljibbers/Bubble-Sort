6bc91993d3a0789f27358543edbf99ce7055f527
tljibbers <aahijoh@gmail.com>
2025-10-08 21:27:55 -0700

    added backend fetching

--- src/front-end/main.js
+++ src/front-end/main.js
@@ -40,6 +40,20 @@ function checkForLength(array)
 
 }
 
+async function backendData() {
+    try {
+            const response = await fetch('/api/bubblesort');
+            if(!response.ok) {
+                throw new Error(response.status);
+            }
+            const data = await response.json()
+
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+}
+
 form.addEventListener('submit', function(event){
     event.preventDefault();
     console.log("test")
@@ -52,7 +66,7 @@ form.addEventListener('submit', function(event){
     {
         bodyContainer.removeChild(form)
 
-        fetch('http://localhost:8080/api/bubblesort/sort', {
+        fetch('/api/bubblesort', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -62,7 +76,9 @@ form.addEventListener('submit', function(event){
         .then(response => response.json())
         .then(data => console.log('Success:', data))
         .catch(error => console.error('Error:', error));
-        
+
+        backendData()
+
         const test = displayArrayOnScreen(arrayConvert)
         setTimeout(applyAnimation(test), 3000)
         
